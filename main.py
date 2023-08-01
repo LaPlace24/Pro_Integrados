e9ae3a3ff30385c01b0aea4d8e543ac25fc0fe2a
LaPlace24 <67650717+LaPlace24@users.noreply.github.com>
2023-07-31 20:46:02 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+# Importando Libreria
+import readchar
 # Pedir el nombre del jugador por teclado
 nombre_jugador = input("Por favor, introduce tu nombre: ")
 
 # Imprimir un mensaje de bienvenida con el nombre
 print("¡Bienvenido,", nombre_jugador + "! Espero que disfrutes jugando.")
+
+# Bucle infinito para precionar teclas
+def main():
+    while True:
+        tecla = readchar.readkey()
+        if tecla == readchar.key.UP:  
+            break
+        print("Tecla presionada:", tecla)
+
+if __name__ == "__main__":
+    print("Presiona cualquier tecla. Presiona la tecla ↑ (UP) para terminar.")
+    main()
