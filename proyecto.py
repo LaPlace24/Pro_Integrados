4ac0694454a3c1e27969ae88dffa31d77200ec2e
LaPlace24 <67650717+LaPlace24@users.noreply.github.com>
2023-07-31 19:43:37 -0500

    Create proyecto.py

---
+++ proyecto.py
@@ -0,0 +1,5 @@
+# Pedir el nombre del jugador por teclado
+nombre_jugador = input("Por favor, introduce tu nombre: ")
+
+# Imprimir un mensaje de bienvenida con el nombre
+print("¡Bienvenido,", nombre_jugador + "! Espero que disfrutes jugando.")
